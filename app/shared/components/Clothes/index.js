51fa111fdab842d23c1a4f74fb957eb1baa5c09b
Sarah Samuel <sarahsamuel@Mac.lan>
2026-03-15 18:13:24 -0700

    create clothes file

---
+++ app/shared/components/Clothes/index.js
@@ -0,0 +1,10 @@
+"use client";
+import { useState } from "react";
+
+//make clothes have same starting point 
+
+//make clothes fall when button clicked
+
+//on click hide svg
+
+//on click gain point
