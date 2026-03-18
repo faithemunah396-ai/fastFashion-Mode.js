629421487508ee1a1659495f72699921e4362ff2
Jvien2 <jvien2@my.bcit.ca>
2026-03-17 17:58:45 -0700

    added svgs

--- app/testJess.js
+++ app/testJess.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import FishGame from "./shared/components/Fish";
+import fishGame from "./shared/components/Fish";
 
 export default function Home() {
+    fishGame();
   return (
-    <div>
-      <FishGame />
-    </div>
+    <div></div>
   );
 }
