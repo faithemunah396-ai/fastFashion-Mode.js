20fa4a4cf493749ab3a28e53c138073cae58e402
Jvien2 <jvien2@my.bcit.ca>
2026-03-17 18:37:36 -0700

    added svgs

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import FishGame from "./shared/components/Fish";
+
+
 
 export default function Home() {
   return (
     <div>
-      <FishGame />
+    
     </div>
   );
 }
