99b1ee50ad850f2c20ed2f28634b112854d6a31f
Sarah Samuel <sarahsamuel@Mac.lan>
2026-03-22 17:20:07 -0700

    fixed help button position

--- app/shared/components/helpButton/index.js
+++ app/shared/components/helpButton/index.js
@@ -2,27 +2,46 @@
 
 function HelpMeButton() {
     return (
+        <div style={{
+        zindex: "3",
+      }}>
         <div
+          style={{
+            position: "absolute",
+            top: "480%",
+            left: "75%",
+            padding: "10px",
+            width: "200px",
+            height: "80px",
+            backgroundColor: "#FFFFFF",
+            borderRadius: "15px",
+            cursor: "pointer",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <div
             style={{
               position: "relative",
               backgroundColor: "#7AC943",
               borderRadius: "10px",
-              border: "8px solid white",
               width: "180px",
               height: "60px",
               padding: "30px",
+
               display: "flex",
               alignItems: "center",
-              justifyContent: "center",
+
               fontFamily: "Nunito, sans-serif",
               fontWeight: "900",
-              fontSize: "20px",
-              cursor: "pointer",
+              fontSize: "32px",
               color: "#20431D",
             }}
           >
-            <p>HELP US</p>
+            Help Us
           </div>
+        </div>
+      </div>
     )
 }
 
